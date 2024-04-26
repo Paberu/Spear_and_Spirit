c85719fc685e77ea9b546a3809081227066ac0d7
Paberu <paberu.tocador@gmail.com>
2024-04-26 20:10:51 +0300

    Update GraphicObject.py

--- GraphicObject.py
+++ GraphicObject.py
@@ -1,26 +1,60 @@
-from time import time
+from abc import ABC, abstractmethod
 
 
-class GraphicObject:
+class GraphicProcessor:
 
-    def __init__(self, image):
+    def visit_static_graphic_element(self, element):
+        return element.get_sprite()
+
+    def visit_dynamic_graphic_element(self, element):
+        sprite = element.get_sprite()
+        current_frame = sprite.get_frame()
+        sprite.next_frame()
+        return current_frame
+
+
+class GraphicObject(ABC):
+
+    def __init__(self, sprite):
         # у простого объекта будет статическая картинка,
         # а у динамического - список картинок.
-        self.image = image
+        self.__sprite = sprite
 
-    def draw(self):
-        return self.image
+    @abstractmethod
+    def accept(self, processor):
+        pass
+
+    def get_sprite(self):
+        return self.__sprite
+
+
+class StaticGraphicObject(GraphicObject):
+
+    def accept(self, processor):
+        processor.process_static_graphic_element(self)
 
 
 class DynamicGraphicObject(GraphicObject):
 
-    def draw(self):
-        timeout = time()
-        return self.image.get_image(timeout)
+    def __init__(self, sprite, size, current_frame):
+        super().__init__(sprite)
+        self.__current_frame = current_frame
+        self.__size = size
+
+    def accept(self, processor):
+        processor.visit_dynamic_graphic_element(self)
+
+    def get_frame(self):
+        return self.__current_frame
+
+    def next_frame(self):
+        self.__current_frame += 1
+        if self.__current_frame == self.__size:
+            self.__current_frame = 0
 
 
 # ну и для примера пару базовых объектов
-class Terra(GraphicObject):
+class Terra(StaticGraphicObject):
     pass
 
 
