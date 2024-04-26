27ad463e274997d408935a27bcf799d817571a13
Paberu <paberu.tocador@gmail.com>
2024-04-26 19:21:49 +0300

    Update GraphicObject.py

--- GraphicObject.py
+++ GraphicObject.py
@@ -1,7 +1,28 @@
-from abc import ABC
+from time import time
 
 
-class GraphicObject(ABC):
+class GraphicObject:
 
     def __init__(self, image):
+        # у простого объекта будет статическая картинка,
+        # а у динамического - список картинок.
         self.image = image
+
+    def draw(self):
+        return self.image
+
+
+class DynamicGraphicObject(GraphicObject):
+
+    def draw(self):
+        timeout = time()
+        return self.image.get_image(timeout)
+
+
+# ну и для примера пару базовых объектов
+class Terra(GraphicObject):
+    pass
+
+
+class Sea(DynamicGraphicObject):
+    pass
